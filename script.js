07b4546cd9bafa90d249dcdb6a35e2b61c98611f
vinicius flores <viniciusflores379@gmail.com>
2024-04-10 14:33:27 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMode() {
+  const html = document.documentElement;
+  html.classList.toggle('light')
+
+  const img = document.querySelector('#profile img')
+
+
+  if (html.classList.contains('light')) {
+    // html.classList.remove('light');
+    img.setAttribute('src', './assets/avatar-light.png')
+  } else {
+    img.setAttribute('src', './assets/avatar.png')
+    // html.classList.add('light')
+  }
+
+
+
+
+}
